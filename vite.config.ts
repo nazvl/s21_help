dd304ca946207fff03dadc9784bd27a5ca259cf6
Vladimir N. <lismiroslav@yandex.ru>
2025-06-27 13:21:36 +0300

    feat: update dependencies and add PWA support
    - Added `vite-plugin` and `vite-plugin-pwa` to dependencies.
    - Downgraded `vite` from version 7.0.0 to 6.3.5.
    - Removed unused `counter` store.
    - Deleted `AboutView.vue` and `HomeView.vue` components.
    - Updated `vite.config.ts` to include PWA configuration and removed vue-devtools plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,35 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 's21 Helper',
+        short_name: 's21',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ecf6ec',
+        theme_color: '#ecf6ec',
+        icons: [
+          { src: '/logo.png', sizes: '192x192', type: 'image/png' },
+          { src: '/logo.png', sizes: '512x512', type: 'image/png' },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setup.js'],
   },
 })
