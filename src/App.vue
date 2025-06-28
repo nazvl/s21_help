793107dff881e3a17c6cbc256d626ac5c8834f07
Vladimir N. <lismiroslav@yandex.ru>
2025-06-29 01:06:00 +0300

    made bottom NavigationBar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import NavigationBar from '@/components/NavigationBar.vue'
+import { useRoute } from 'vue-router'
+const route = useRoute();
+
 </script>
 
 <template>
   <RouterView />
+  <NavigationBar v-if="route.path !== '/login'"/>
 </template>
 
 <style scoped>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { HomeIcon, UserIcon, CalendarIcon, AcademicCapIcon } from '@heroicons/vue/24/outline'
+import type { Component } from 'vue'
+const router = useRouter();
+
+interface Link {
+  name: string
+  to: string
+  icon: Component
+}
+
+const links: Link[] = [
+  {
+    name: 'Home',
+    to: '/home',
+    icon: HomeIcon,
+  },
+  {
+    name: 'Profile',
+    to: '/profile',
+    icon: UserIcon,
+  },
+  {
+    name: 'Calendar',
+    to: '/calendar',
+    icon: CalendarIcon,
+  },
+  {
+    name: 'Projects',
+    to: '/projects',
+    icon: AcademicCapIcon,
+  },
+]
+
+function onNavigateTo(to: string) {
+  console.log(to);
+  router.push(to);
+}
+</script>
+
+<template>
+  <div class="fixed bottom-0 w-full h-16 bg-lightgray-900 border-t border-t-lightgray-300">
+    <ul class="flex items-center h-full text-justwhite-500 justify-around">
+      <li v-for="link in links" :key="link.name">
+        <button class="flex items-center gap-[5px] justify-center flex-col" @click="onNavigateTo(link.to)">
+          <component :is="link.icon" class="w-6 h-6" />
+          <p class="text-xs text-lightgray-300">{{ link.name }}</p>
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
